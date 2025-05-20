05ed69e9abfdeef97f6c0a90dd2ef36f66357bd0
fertiago <etiago@mailo.com>
2025-05-20 19:56:33 +0200

    adding icons and the react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './pages/App.jsx'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Contact from './pages/Contact'
+import Calculatrice from './pages/Calculatrice'
+import './styles/index.css'
+import './styles/themes.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header/>
+      <main>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/contact/" element={<Contact />} />
+          <Route path="/calculatrice/" element={<Calculatrice />} />
+        </Routes>
+      </main>
+      <Footer/>
+    </BrowserRouter>
   </StrictMode>,
 )
